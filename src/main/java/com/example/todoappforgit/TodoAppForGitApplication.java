f428ee9dd637765f81621390bd7ab175b544f3c7
k2taee <rlxo1992@gmail.com>
2023-08-07 16:33:32 +0900

    initial

---
+++ src/main/java/com/example/todoappforgit/TodoAppForGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoappforgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppForGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppForGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todoappforgit/TodoAppForGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoappforgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppForGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
